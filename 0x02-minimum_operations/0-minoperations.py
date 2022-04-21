8942ac95f5d5ea4891a7a83e5282a8dbb7e3f5cb
betascribbles <firdaushassan033@gmail.com>
2022-04-21 22:41:50 +0300

    added documentation

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
+'''Minimum Operations python3 challenge'''
 
 
 def minOperations(n):
+    '''calculates the fewest number of
+    operations needed to result in exactly n H
+    characters in this file.
+    Returns:
+        Integer : if n is impossible to achieve, return 0
+    '''
     pasted_chars = 1  # how many chars in the file
     clipboard = 0  # how many H's copied
     counter = 0  # operations counter
